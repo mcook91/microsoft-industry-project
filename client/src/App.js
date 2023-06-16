d59b83a83bfe8b6d7bb62365928547d81b16866e
Mitch Cook <cookmitchell67@gmail.com>
2023-06-16 14:23:37 -0400

    complete card feature

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import PrivacySettings from './components/PrivacySettings/PrivacySettings'
+import EventInfo from './components/EventInfo/EventInfo';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Nav />
       <PrivacySettings />
       <Routes>
-        <Route path="/"  />
+        <Route path="/edit" element={<EventInfo />} />
       </Routes>
       </BrowserRouter>
     </div>
---
+++ client/src/components/EventCard/EventCard.js
@@ -0,0 +1,51 @@
+import deleteIcon from '../../Assets/Icons/Delete.svg'
+import locationIcon from '../../Assets/Icons/Location.svg'
+import React, { useState } from 'react'
+import './EventCard.scss'
+
+const EventCard = ( {card, setActiveImg}) => {
+    const [active, setActive] = useState(true)
+    const onClickHandler = () => {
+        if (active) {
+            setActive(false)
+            setActiveImg(false)
+        } else {
+            setActive(true)
+            setActiveImg(true)
+        }
+    }
+
+    return (
+        <div className='eventcard'>
+            <div className='eventcard__container-header'>
+                <h3 className='eventcard__title'>{card.eventName}</h3>
+                <p className='eventcard__date'>{card.date}</p>
+            </div>
+            <div className='eventcard__container-card'>
+                <div className='eventcard__container'>
+                    <div className='eventcard__container-info'>
+                        <h4 className='eventcard__title-sub'>Title</h4>
+                        <p className='eventcard__info'>{card.eventInfo}</p>
+                    </div>
+                    <img src={deleteIcon} alt='Delete Icon' className='eventcard__delete' />
+                </div>
+                <div className='eventcard__container'>
+                    <div className='eventcard__container-info'>
+                        <h4 className='eventcard__title-sub'>Location</h4>
+                        <div className='eventcard__container-info eventcard__container-sub'>
+                            <img src={locationIcon} alt='Location Icon' className='eventcard__location' />
+                            <p className='eventcard__info'>{active ? card.locationInfo : '---'}</p>
+                        </div>
+                    </div>
+                    <img onClick={onClickHandler} src={active ? deleteIcon : 'placeholder'} alt='Delete Icon' className='eventcard__delete' />
+                </div>
+            </div>
+            <div className='eventcard__footer-container'>
+                <p className='eventcard__footer'>View Details</p>
+                <p className='eventcard__footer'>Delete All</p>
+            </div>
+        </div>
+    )
+}
+
+export default EventCard
