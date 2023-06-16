ba0649a903e4ac02beea71c0792765739d7d2882
Eric Tom <erictom715@gmail.com>
2023-06-16 12:13:42 -0400

    finished the microsoftNav

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import './App.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import PrivacySettings from './components/PrivacySettings/PrivacySettings'
+import MicrosoftNav from './components/MicrosoftNav/MicrosoftNav';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <MicrosoftNav />
       <Nav />
       <PrivacySettings />
       <Routes>
